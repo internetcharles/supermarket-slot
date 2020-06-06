a81e3a0316f5373e3816f417e147af6eef430585
internetcharles <charlesodellsmith@gmail.com>
2020-06-06 12:11:03 -0700

    snimations

--- Assets/Scripts/ReelCreator.cs
+++ Assets/Scripts/ReelCreator.cs
@@ -23,67 +23,36 @@ public class ReelCreator : MonoBehaviour
     private Vector3 reelOneTargetPosition = new Vector3(0, -15);
     private Vector3 reelTwoTargetPosition = new Vector3(0, -15);
     private Vector3 reelThreeTargetPosition = new Vector3(0, -15);
-    private int targetYPosition = -15;
 
     private int totalScore = 0;
     public bool slotsSpun = false;
+    public bool slotsSpinning = false;
+
+    private Animation anim;
+
     // Start is called before the first frame update
 
     void Start()
     {
-        Transform thisTransform = GetComponent<Transform>();
-        slotsSpun = true;
-        SlotsPicker();
-        InstantiateIcons();
 
+
+        anim = gameObject.GetComponent<Animation>();
+        anim["SlotSpinReelOne"].layer = 100;
+        anim["SlotSpinReelTwo"].layer = 100;
+        anim["SlotSpinReelThree"].layer = 100;
+
+
+        slotsSpun = true;
     }
+
     void Update()
     {
-        StartCoroutine(AnimateSlots());
     }
 
 
     private void InstantiateIcons()
     {
-        /*
-        Vector3 slot1Position1 = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        Vector3 slot1Position2 = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z);
-        Vector3 slot1Position3 = new Vector3(transform.position.x, transform.position.y + 4, transform.position.z);
-        Vector3 slot1Position4 = new Vector3(transform.position.x, transform.position.y + 6, transform.position.z);
-        Vector3 slot1Position5 = new Vector3(transform.position.x, transform.position.y + 8, transform.position.z);
-        Vector3 slot2Position = new Vector3(transform.position.x + 2, transform.position.y, transform.position.z);
-        Vector3 slot2Position2 = new Vector3(transform.position.x + 2, transform.position.y + 2, transform.position.z);
-        Vector3 slot2Position3 = new Vector3(transform.position.x + 2, transform.position.y + 4, transform.position.z);
-        Vector3 slot2Position4 = new Vector3(transform.position.x + 2, transform.position.y + 6, transform.position.z);
-        Vector3 slot2Position5 = new Vector3(transform.position.x + 2, transform.position.y + 8, transform.position.z);
-        Vector3 slot3Position1 = new Vector3(transform.position.x + 4, transform.position.y, transform.position.z);
-        Vector3 slot3Position2 = new Vector3(transform.position.x + 4, transform.position.y + 2, transform.position.z);
-        Vector3 slot3Position3 = new Vector3(transform.position.x + 4, transform.position.y + 4, transform.position.z);
-        Vector3 slot3Position4 = new Vector3(transform.position.x + 4, transform.position.y + 6, transform.position.z);
-        Vector3 slot3Position5 = new Vector3(transform.position.x + 4, transform.position.y + 8, transform.position.z);
 
-        for (int i = 0; i < ReelLength; i++)
-        {
-            {
-                Instantiate(reel1[0], slot1Position1, Quaternion.identity);
-                Instantiate(reel1[1], slot1Position2, Quaternion.identity);
-                Instantiate(reel1[2], slot1Position3, Quaternion.identity);
-                Instantiate(reel1[3], slot1Position4, Quaternion.identity);
-                Instantiate(reel1[4], slot1Position5, Quaternion.identity);
-
-                Instantiate(reel2[0], slot2Position, Quaternion.identity);
-                Instantiate(reel2[1], slot2Position2, Quaternion.identity);
-                Instantiate(reel2[2], slot2Position3, Quaternion.identity);
-                Instantiate(reel2[3], slot2Position4, Quaternion.identity);
-                Instantiate(reel2[4], slot2Position5, Quaternion.identity);
-
-                Instantiate(reel3[0], slot3Position1, Quaternion.identity);
-                Instantiate(reel3[1], slot3Position2, Quaternion.identity);
-                Instantiate(reel3[2], slot3Position3, Quaternion.identity);
-                Instantiate(reel3[3], slot3Position4, Quaternion.identity);
-                Instantiate(reel3[4], slot3Position5, Quaternion.identity);
-            }
-        */
         var newReelOne = GameObject.Find("Reel One").transform;
         var newReelTwo = GameObject.Find("Reel Two").transform;
         var newReelThree = GameObject.Find("Reel Three").transform;
@@ -170,6 +139,9 @@ public class ReelCreator : MonoBehaviour
 
     void CheckResults()
     {
+        var newReelOne = GameObject.Find("Reel One").transform;
+        var newReelTwo = GameObject.Find("Reel Two").transform;
+        var newReelThree = GameObject.Find("Reel Three").transform;
         int thirdToLastReel = ReelLength - 3;
         if (reel1[thirdToLastReel] == reel2[thirdToLastReel] && reel1[thirdToLastReel] == reel3[thirdToLastReel])
         {
@@ -190,34 +162,6 @@ public class ReelCreator : MonoBehaviour
         Debug.Log(totalScore);
     }
 
-    IEnumerator AnimateSlots()
-    {
-        var newReelOne = GameObject.Find("Reel One").transform;
-        var newReelTwo = GameObject.Find("Reel Two").transform;
-        var newReelThree = GameObject.Find("Reel Three").transform;
-
-        if (slotsSpun == true)
-        {
-            newReelOne.transform.Translate(Vector3.down * 3 * Time.deltaTime);
-            yield return new WaitForSeconds(0.2f);
-            newReelTwo.transform.Translate(Vector3.down * 3 * Time.deltaTime);
-            yield return new WaitForSeconds(0.2f);
-            newReelThree.transform.Translate(Vector3.down * 3 * Time.deltaTime);
-        }
-
-        if (newReelOne.position.y <= targetYPosition)
-        {
-            newReelOne.position = reelOneTargetPosition;
-        }
-        if (newReelTwo.position.y <= targetYPosition)
-        {
-            newReelTwo.position = reelTwoTargetPosition;
-        }
-        if (newReelThree.position.y <= targetYPosition)
-        {
-            newReelThree.position = reelThreeTargetPosition;
-        }
-    }
 
     void TurnSlotsOff()
     {
--- Assets/Scripts/ReelThree.cs
+++ Assets/Scripts/ReelThree.cs
@@ -2,17 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReelOne : MonoBehaviour
+public class ReelThree : MonoBehaviour
 {
+    // Start is called before the first frame update
     private Animation anim;
 
     void Start()
     {
         anim = gameObject.GetComponent<Animation>();
-        anim["SlotSpinReelOne"].layer = 100;
-        anim.Play("SlotSpinReelOne");
+        anim["SlotSpinReelThree"].layer = 100;
+        anim.Play("SlotSpinReelThree");
     }
-
-
-
 }
